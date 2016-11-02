9a466223eb45bc10c9ca56497cf56511c50a710c
Atora <at@atora.dev>
2016-11-02 15:19:35 -0400

    added all the code

---
+++ index.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+//This entire thing can probably more elegant but meh
+if (process.argv.length === 4) {
+	var flags = '';
+	var folderToSearch = process.argv[2];
+	var regex = process.argv[3];
+} else if (process.argv.length === 5) {
+	var flags = process.argv[2].toLowerCase();
+	var folderToSearch = process.argv[3];
+	var regex = process.argv[4];
+}else{
+	console.log('search [-qdfr] "directory/to/search" "some|(regex)"');
+	console.log('-q: Quiet mode, don\'t output anything');
+	console.log('-d: Dry run, don\'t save to a directory (Can be used to count the amount of matches)');
+	console.log('-f: Silence errors');
+	console.log('-r: Recursive Search, search subdirectories of the directory');
+	process.exit(0); 
+}
+
+const fs   = require('fs');
+const path = require('path');
+const resultsFolder = path.join('.', 'results');
+
+if (!flags.includes('d')) {
+	if (fs.existsSync(resultsFolder)) {
+		var oldFiles = fs.readdirSync(resultsFolder);
+		for(file of oldFiles) {
+			try {
+				fs.unlinkSync(path.join('.', 'results', file));
+			} catch (e) {if (!flags.includes('f')) console.log(e)}
+		}
+		fs.rmdirSync(resultsFolder);
+	}
+	fs.mkdirSync(resultsFolder);
+}
+try {
+	if (!flags.includes('r')) {
+		var files = fs.readdirSync(folderToSearch);
+	} else {
+		var files = walkSync(folderToSearch); //Recursive search, search subdirectories of the folder
+	}
+} catch (e) {
+	if (!flags.includes('f')) console.log('No folder found!');
+	process.exit(0);
+}
+
+var results = 0;
+var filesSearched = 0;
+for (file of files) {
+	try {
+		var fileName = file.match(/(.*)\/(.*)$/)[2] || file;
+		if (fileName.search(regex) !== -1) {
+			results++;
+			if (!flags.includes('d')) fs.linkSync(path.join(folderToSearch, file), path.join(resultsFolder, fileName));
+		}
+	} catch (e) {if (!flags.includes('f')) console.log(`Couldn\'t link file/directory "${file}"`)}
+	filesSearched++;
+	process.stdout.write(loadingBar(filesSearched, files.length, process.stdout.columns - 8));
+	process.stdout.cursorTo(0);
+}
+process.stdout.clearLine();
+if (!flags.includes('q')) console.log(`Found ${results} result${results !== 1 ? 's' : ''}`);
+
+if (results === 0 && !flags.includes('d')) fs.rmdirSync(resultsFolder);
+
+function walkSync(dir, filelist) {
+  var path = path || require('path');
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      filelist = walkSync(path.join(dir, file), filelist);
+    }
+    else {
+      filelist.push(path.join(dir, file));
+    }
+  });
+  return filelist;
+};
+
+function loadingBar(current, max, width) {
+	var currentPercent = current/ max;
+	var filled = '#'.repeat(Math.floor(currentPercent * width));
+	var empty  = ' '.repeat(width - Math.floor(currentPercent * width));
+	
+	return '[' + filled + empty + ']' + ' ' + Math.floor(currentPercent*1000)/10 + '%';
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+# name-search
+
+Search a directory for files that match a regex and copies them to a directory called "results". Supports recursion for subdirectories.
+
+```
+nsearch [-qdfr] "directory/to/search" "some|(regex)"
+-q: Quiet mode, don't output anything
+-d: Dry run, don't save to a directory (Can be used to count the amount of matches)
+-f: Silence errors
+-r: Recursive Search, search subdirectories of the directory
+```
+
+## Installation
+
+`npm i -g name-search`
+or
+`npm install --global name-search`
+
+Depends on your laziness
+
+## Usage
+
+`nsearch . tree`
+
+Search the current directory for file names containing "tree". Places results in "./results".
+
+`nsearch -rd . apple`
+
+Seach the current directory and all subdirectories for file names containing "apple". Don't place results in "./results".
+
+`nsearch -r ./trees maple|apple`
+
+Search "./trees" and it's subdirectories for filenames matching "maple" or "apple". Place results in "./results".
+
+## Notes
+
+`name-search` will delete the "./results" directory if it exists, so be careful.
+
+Also I wrote most of this at 1 am and wrote the recurion and options part while tired so be careful with this.
